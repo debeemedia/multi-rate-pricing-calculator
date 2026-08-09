cba8f70933cf7b0c33726d2c0dd8ca537c67805d
Deborah Okeke <okekedeborah@gmail.com>
2026-08-09 13:14:35 +0100

    refactor: make the `id` column on users table a string

--- database/migrations/1758943358073_create_users_table.ts
+++ database/migrations/1758943358073_create_users_table.ts
@@ -5,13 +5,13 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.string('id').notNullable().primary()
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
 
       table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').nullable()
+      table.timestamp('updated_at').notNullable()
     })
   }
 
